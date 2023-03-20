533ce7f851b350beca72eb24fef516137fa3c77f
Om Mishra <om.2472004@gmail.com>
2023-03-20 21:43:51 +0530

    boo hoo

---
+++ website.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template,request
+import uuid
+import datetime
+from waitress import serve
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    quote = requests.get('https://api.quotable.io/random').json()["content"]
+    return render_template('index.html',uuid=uuid.uuid4(),user_ip=request.headers.get('X-Forwarded-For', request.remote_addr),year=datetime.datetime.today().year,quote=quote)
+
+@app.route('/ping',methods=['GET','POST'])
+def ping():
+    return ('pong')
+
+if __name__ == '__main__':
+    serve(app,host='0.0.0.0',port=80)
