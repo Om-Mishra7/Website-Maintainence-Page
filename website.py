ea0f4d5dd1d4f5c686f36dbbbd370fb06ee95cca
Om Mishra <admin@projectrexa.ml>
2023-07-29 19:38:58 +0000

    Updates

--- website.py
+++ website.py
@@ -1,24 +1,27 @@
-from flask import Flask,render_template,request
-import uuid
-import datetime
-from waitress import serve
-import requests
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
-    quote = requests.get('https://api.quotable.io/random').json()["content"]
-    return render_template('index.html',uuid=uuid.uuid4(),user_ip=request.headers.get('X-Forwarded-For', request.remote_addr),year=datetime.datetime.today().year,quote=quote)
+    return render_template('index.html')
 
-@app.route('/ping',methods=['GET','POST'])
-def ping():
-    return ('pong')
 
-@app.route('/webhook',methods=['GET','POST'])
-def webhook():
-    return ('okay')
+@app.route('/terms-of-service')
+def terms_of_service():
+    return render_template('terms-of-service.html')
+
+
+@app.route('/privacy-policy')
+def privacy_policy():
+    return render_template('privacy-policy.html')
+
+
+@app.route('/8f9e02a142c704757466060cf57d4552.html')
+def host():
+    return render_template('8f9e02a142c704757466060cf57d4552.html')
+
 
 if __name__ == '__main__':
-    serve(app,host='0.0.0.0',port=80)
+    app.run(debug=True)
